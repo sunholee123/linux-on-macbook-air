2a712106f2aade00b42d9f3a162b6b81e717b4ca
Sunho Lee <sunho.s.lee@gmail.com>
2023-11-30 01:03:57 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 apt install vim htop xserver-xorg-input-mtrack mbpfan
-apt purge thunderbird hexchat rhythmbox pix
+apt purge thunderbird hexchat rhythmbox pix transmission-common drawing simple-scan gnome-calendar hypnotix system-config-printer
+apt autoremove
